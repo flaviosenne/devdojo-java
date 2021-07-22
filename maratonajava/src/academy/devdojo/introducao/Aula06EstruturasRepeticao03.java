958d527f9ef96e627bb96f4df80f3319aff68c85
joao flavio <flaviosenne@gmail.com>
2021-07-22 11:20:37 -0300

    class 29

---
+++ maratonajava/src/academy/devdojo/introducao/Aula06EstruturasRepeticao03.java
@@ -0,0 +1,14 @@
+package academy.devdojo.introducao;
+
+public class Aula06EstruturasRepeticao03 {
+    public static void main(String[] args) {
+        int i =0;
+        while (i< 50){
+            System.out.println(i);
+            if(i ==25){
+                break;
+            }
+            i++;
+        }
+    }
+}
