7b31819d06671fce6089569adb4fee70b8e5337b
joao flavio <flaviosenne123@gmail.com>
2021-08-11 11:36:51 -0300

    class 72

--- maratonajava/src/academy/devdojo/javacore/Hheranca/dominio/Funcionario.java
+++ maratonajava/src/academy/devdojo/javacore/Hheranca/dominio/Funcionario.java
@@ -3,6 +3,12 @@ package academy.devdojo.javacore.Hheranca.dominio;
 public class Funcionario extends Pessoa{
     private double salary;
 
+    public void print(){
+        super.print();
+        System.out.println("Salario: "+ salary);
+
+
+    }
     public double getSalary() {
         return salary;
     }
